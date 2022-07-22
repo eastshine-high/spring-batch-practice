7bc1b5a66e8262bd951df99e3c4916b19be22b41
eastshine-high <eastshine.high@gmail.com>
2022-07-22 17:12:44 +0900

    project initialization

---
+++ src/main/java/com/eastshine/practice/SpringBatchPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.eastshine.practice;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchPracticeApplication.class, args);
+	}
+}
