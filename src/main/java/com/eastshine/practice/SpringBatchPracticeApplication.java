e966a4cdcd5bebbf1b42325b4aea1a38dc47f550
eastshine-high <eastshine.high@gmail.com>
2022-07-22 18:00:39 +0900

    hello spring batch 출력해 보기

--- src/main/java/com/eastshine/practice/SpringBatchPracticeApplication.java
+++ src/main/java/com/eastshine/practice/SpringBatchPracticeApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableBatchProcessing // 본 애플리케이션이 배치 프로세싱을 하겠다는 것을 의미
 public class SpringBatchPracticeApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/eastshine/practice/hello/HelloConfiguration.java
@@ -0,0 +1,42 @@
+package com.eastshine.practice.hello;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Slf4j
+public class HelloConfiguration {
+
+    private JobBuilderFactory jobBuilderFactory; // SpringBatch 설정으로 빈으로 주입받을 수 있다.
+    private StepBuilderFactory stepBuilderFactory;
+
+    public HelloConfiguration(JobBuilderFactory jobBuilderFactory, StepBuilderFactory stepBuilderFactory) {
+        this.jobBuilderFactory = jobBuilderFactory;
+        this.stepBuilderFactory = stepBuilderFactory;
+    }
+
+    @Bean
+    public Job helloJob() { // Job은 배치의 실행 단위이다.
+        return jobBuilderFactory.get("helloJob") // name은 스프링 배치를 실행할 수 있는 key이키도 하다.
+                .incrementer( // 실행 단위를 구분,
+                        new RunIdIncrementer()) // Job이 실행할 때 마다, parameter id를 자동으로 생성
+                .start(helloStep()) // Job 실행 시 최초로 실행될 스텝을 설정
+                .build();
+    }
+
+    @Bean
+    public Step helloStep() { // Job의 실행 단위
+        return stepBuilderFactory.get("helloStep")
+                .tasklet((contribution, chunkContext) -> {
+                    log.info("hello spring batch!");
+                    return RepeatStatus.FINISHED;
+                }).build();
+    }
+}
